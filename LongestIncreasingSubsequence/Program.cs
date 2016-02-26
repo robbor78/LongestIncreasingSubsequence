ba90e603bcc17793f6cd07aba29457bb3baf72c4
username <email@email.com>
2016-02-26 14:33:51 +0200

    longest increasing subsequence topcoder

---
+++ LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestIncreasingSubsequence
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+
+    public int getContinuous(int[] sequence, out int[] subsequence)
+    {
+      int length = sequence.Length;
+      int[] solutions = Enumerable.Repeat(1, length).ToArray();
+      int max = 0;
+      int bestSolution = -1;
+
+      for (int i = 0; i < length; i++)
+      {
+        int j = i - 1;
+        if (j >= 0 && sequence[j] <= sequence[i])
+        {
+          solutions[i] = solutions[j] + 1;
+          if (solutions[i] > max)
+          {
+            max = solutions[i];
+            bestSolution = i;
+          }
+        }
+      }
+
+      if (bestSolution == -1)
+      {
+        subsequence = null;
+      }
+      else
+      {
+        int subsequenceLength = solutions[bestSolution];
+        int start = bestSolution - subsequenceLength + 1;
+        subsequence = sequence.Skip(start).Take(subsequenceLength).ToArray();
+      }
+
+      return bestSolution == -1 ? -1 : max;
+    }
+
+    public int getNonContinuous(int[] sequence, out int[] subsequence)
+    {
+      int length = sequence.Length;
+      int[] solutions = Enumerable.Repeat(1, length).ToArray();
+      int max = 0;
+      int bestSolution = -1;
+
+      for (int i = 0; i < length; i++)
+      {
+        int currMax = 0;
+        for (int j = i - 1; j >= 0; j--)
+        {
+
+          if (sequence[j] <= sequence[i])
+          {
+            if (solutions[j] > currMax)
+            {
+              currMax = solutions[j];
+            }
+          }
+        }
+
+        solutions[i] += currMax;
+        if (solutions[i] > max)
+        {
+          bestSolution = i;
+          max = solutions[i];
+        }
+      }
+
+      if (bestSolution == -1 || solutions[bestSolution] == 1)
+      {
+        subsequence = null;
+      }
+      else
+      {
+        //subsequence = null;
+        int prev = sequence[bestSolution];
+        int count = solutions[bestSolution];
+        Queue<int> queue = new Queue<int>();
+        for (int i = bestSolution; i >= 0; i--)
+        {
+          if (sequence[i] <= prev && solutions[i] == count)
+          {
+            queue.Enqueue(sequence[i]);
+            prev = sequence[i];
+            count = solutions[i] - 1;
+          }
+        }
+        subsequence = queue.Reverse().ToArray();
+      }
+
+      return bestSolution == -1 || solutions[bestSolution] == 1 ? -1 : max;
+    }
+  }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LongestIncreasingSubsequence;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestContinuous()
+    {
+      int[][] allSequences = new int[][]
+      {
+        new int[] { 5,3,4,8,6,7},
+        new int[] { 1,2,3,4,5,6},
+        new int[] { 6,5,4,3,2,1},
+        new int[] { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 }
+      };
+      int[] allLength = new int[] { 3, 6, -1, 2 };
+      int[][] allSubsequences = new int[][]
+      {
+        new int[] { 3,4,8},
+        new int[] { 1,2,3,4,5,6},
+        null,
+        new int[] {0,8 }
+      };
+
+      Program p = new Program();
+      int length = allSequences.Length;
+      for (int i = 0; i < length; i++)
+      {
+        int[] sequence = allSequences[i];
+        int[] subsequence = null;
+        int actual = p.getContinuous(sequence, out subsequence);
+        int expected = allLength[i];
+        Assert.AreEqual(expected, actual);
+        TestEqual(allSubsequences[i], subsequence);
+      }
+
+    }
+
+    [TestMethod]
+    public void TestNonContinuous()
+    {
+      int[][] allSequences = new int[][]
+      {
+        new int[] { 5,3,4,8,6,7},
+        new int[] { 1,2,3,4,5,6},
+        new int[] { 6,5,4,3,2,1},
+        new int[] { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 }
+      };
+      int[] allLength = new int[] { 4, 6, -1, 6 };
+      int[][] allSubsequences = new int[][]
+      {
+        new int[] { 3,4,6,7},
+        new int[] { 1,2,3,4,5,6},
+        null,
+        new int[] {0, 2, 6, 9, 11, 15}
+      };
+
+      Program p = new Program();
+      int length = allSequences.Length;
+      for (int i = 0; i < length; i++)
+      {
+        int[] sequence = allSequences[i];
+        int[] subsequence = null;
+        int actual = p.getNonContinuous(sequence, out subsequence);
+        int expected = allLength[i];
+        Assert.AreEqual(expected, actual);
+        TestEqual(allSubsequences[i], subsequence);
+      }
+    }
+
+    private void TestEqual(int[] expected, int[] actual)
+    {
+      if (expected == null)
+      {
+        Assert.IsNull(actual);
+      }
+      else {
+        Assert.AreEqual(expected.Length, actual.Length);
+        Assert.IsTrue(expected.SequenceEqual(actual));
+      }
+    }
+  }
+}
